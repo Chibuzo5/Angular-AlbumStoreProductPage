3deb4eac97231eb90053990c0f1c1db9f4806c62
chibuzo5 <chieezy5@gmail.com>
2019-10-28 13:49:58 +0100

    fixed some errors

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,10 +3,9 @@ import {Http, Response} from '@angular/http'
 import 'rxjs/add/operator/map';
 @Injectable()
 export class ProductService {
-private _albumUrl = './assets/album.json'
+private _albumUrl = '../assets/album.json';
   constructor(private _http: Http) { }
-   getAlbum(id:number){
-     return this._http.get(this._albumUrl).map((response) =>
-     response.json());
+   getAlbum(id: number) {
+     return this._http.get(this._albumUrl).map((response) => response.json());
    }
 }
